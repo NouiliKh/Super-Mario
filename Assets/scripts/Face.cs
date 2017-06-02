1ee38aa64e5b4b45406cc67ecf038d514cb19e3f
NouiliKh <khalil.nouili@hotmail.com>
2017-06-02 21:54:52 +0100

    begin

---
+++ Assets/scripts/Face.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FACE_TYPE { Star = 0, Mario = 1, Luigi = 2, Peach = 3, Toad = 4, Bowser = 5 };
+[System.Serializable]
+
+public class Face  {
+    public FACE_TYPE faceTyoe;
+    public GameObject facePrefab;
+}
---
+++ Assets/scripts/Machine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Machine : MonoBehaviour {
+
+    public int numSlots;
+    public float minSlotSpeed;
+    public float maxSlotSpeed;
+    public GameObject slotPefab;
+    private bool isSpinning = false;
+    public Face[] faces;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
